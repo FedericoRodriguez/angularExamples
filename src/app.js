9dc80953bcf1dda0416041377bc30698d873353e
FedericoR <federicorodriguez1989@gmail.com>
2017-02-06 02:34:23 -0300

    Http request

--- src/app.js
+++ src/app.js
@@ -18,4 +18,22 @@ app.controller("HomeController",["$scope","$http",function($scope,$http){
 		$scope.comentarios.push($scope.nuevoComentario);
 		$scope.nuevoComentario = {texto: "Comentario",autor: "Nombre del Autor"};
 	}
+//post
+	$scope.posts = {};
+	$http.get("http://localhost:3000/posts").then(function(response) {
+        $scope.posts = response.data;
+    });
+    $scope.nuevoPost={};
+
+	$scope.agregarPost = function(){
+
+		$http.post("http://localhost:3000/posts",$scope.nuevoPost).then(function (response) {
+			$scope.posts.push(response.data);
+			$scope.nuevoPost={};
+		});
+	}
+	//tambien se puede usar $scope.$watchCollection para escuchar sobre la  coleccion posts y ejecutar una funcion.
+		
+
 }]);
+//9
