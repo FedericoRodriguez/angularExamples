453c78be45a0d379ba6f8317070036151722b67e
FedericoR <federicorodriguez1989@gmail.com>
2017-02-19 23:16:22 -0300

    Teoria de run, aplly,digest,watchers

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -4,5 +4,16 @@ app.controller("HomeController",["$scope","$location",function($scope,$location)
 	$scope.bandera_hover = false;
 	$scope.bandera_hover2 = false;
 	$scope.myStyle = {};
+
+	//apply
+	$('#cambiarTitulo').click(function(){
+		//USAR NG-CLICK EN VEZ DE APPLY
+		$scope.$apply(function(){
+			//forma correcta, aplly te introduce en el scope y llama a $digest() -> watchers
+			$scope.title="Hola a todos!!!";
+		});
+		//$scope.$digest();
+		//$scope.$apply();//aca afuera si hay errores angular no se entera porque esta operando fuera del scope.
+	});
 	
 }]);
